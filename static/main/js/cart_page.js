a22a1d34cd0f796b5b7675e1d679b18edf17290c
Xamelllion <sid.vp04@gmail.com>
2023-07-15 15:31:19 +0300

    add placeholder to empty cart page add total price counter (evaluates using localStorage values) // final price must be evaluate on backend add payment button to cart page (like in popup)

--- static/main/js/cart_page.js
+++ static/main/js/cart_page.js
@@ -1,6 +1,9 @@
 let product_list_div = document.querySelector('.product_list');
 let cart_counter = document.querySelector('.cart_counter');
+let final_price = document.querySelector('.final_price');
 
+let empty_cart_div = document.querySelector('.empty_cart');
+let cart_page_wrap_div = document.querySelector('.cart_page_wrap');
 
 let cart_object = JSON.parse(
     localStorage.getItem('cart')
@@ -26,11 +29,20 @@ String.prototype.format = function() {
     }
     return formatted;
 };
+
+function evaluatePrice() {
+    let total_price = 0;
+    for (let product_id in cart_object) {
+        total_price += parseInt(cart_object[product_id].cost);
+    }
+    return total_price;
+}
 // -----------------
 
 
 
-if (cart_object != null) {
+if (cart_object != null && Object.keys(cart_object).length != 0) {
+    cart_page_wrap_div.classList.remove('deactive');
     for (let product_id in cart_object) {
         product_list_div.innerHTML += item_template.format(
             product_id,
@@ -38,7 +50,7 @@ if (cart_object != null) {
             cart_object[product_id].cost
         );
     }
-
+    final_price.textContent = evaluatePrice();
 
     let remove_btns = document.querySelectorAll('.remove_div_a');
     remove_btns.forEach((btn) => {
@@ -55,7 +67,16 @@ if (cart_object != null) {
 
             cart_counter.innerText =
                 Object.keys(cart_object).length;
+            
+            final_price.textContent = evaluatePrice();
+
+            if (Object.keys(cart_object).length == 0) {
+                cart_page_wrap_div.classList.add('deactive');
+                empty_cart_div.classList.remove('deactive');
+            }
         });
     });
-};
+} else {
+    empty_cart_div.classList.remove('deactive');
+}
 
