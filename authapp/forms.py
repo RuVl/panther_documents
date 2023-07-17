f0ffb71308cccf06c87064edf9b773a3d52a3d20
ruvl <ru.vlad.13@gmail.com>
2023-07-17 17:17:41 +0300

    Add placeholders to login and register pages

--- authapp/forms.py
+++ authapp/forms.py
@@ -10,6 +10,13 @@ from authapp.models import ShopUser
 class ShopUserRegisterForm(UserCreationForm):
     captcha = ReCaptchaField()
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs.update({'placeholder': 'Email'})
+        self.fields['username'].widget.attrs.update({'placeholder': 'Username'})
+        self.fields['password1'].widget.attrs.update({'placeholder': 'Password'})
+        self.fields['password2'].widget.attrs.update({'placeholder': 'Repeat password'})
+
     class Meta:
         model = ShopUser
         fields = ('email', 'username', 'password1', 'password2')
@@ -27,6 +34,11 @@ class ShopUserRegisterForm(UserCreationForm):
 
 
 class ShopUserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'placeholder': 'Username'})
+        self.fields['password'].widget.attrs.update({'placeholder': 'Password'})
+
     class Meta:
         model = ShopUser
         fields = ('username', 'password')
