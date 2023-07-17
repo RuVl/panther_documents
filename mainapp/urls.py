c738fb60ef8e4f4464625e9de19959a85db4ac2d
ruvl <ru.vlad.13@gmail.com>
2023-07-17 16:54:21 +0300

    files -> send-links

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,7 +6,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', BookListView.as_view(), name='home'),
-    path('get-file/', SendLinksFormView.as_view(), name='files'),
+    path('get-file/', SendLinksFormView.as_view(), name='send-links'),
     path('get-file/<str:email>/<str:security_code>', DownloadLinksView.as_view(), name='download'),
     path('cart/', CartView.as_view(), name='cart')
 ]
