7ae0d8e1a4538ce06459d49d5645798fee6c4562
ruvl <ru.vlad.13@gmail.com>
2023-07-17 17:38:34 +0300

    Url file test

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
+from mainapp.models import Product, Country
+
+
 # Create your tests here.
+# noinspection PyMethodMayBeStatic
+class ProductTest(TestCase):
+    def test_files_url(self):
+        c = Country(title='TEST')
+        c.save()
+
+        product = Product(count=3, title='test', usd_cost=5, rub_cost=40, country=c, file='products/test.zip')
+        product.save()
+
+        print(product.file.url)
