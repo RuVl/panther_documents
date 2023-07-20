0b04914cf6f263cb9c446d33b79c6943e2199d9a
Xamelllion <sid.vp04@gmail.com>
2023-07-20 16:46:17 +0300

    send pay form [test]

--- static/cart/js/cart_page.js
+++ static/cart/js/cart_page.js
@@ -1,13 +1,20 @@
-let product_list_div = document.querySelector('.product_list');
-let cart_counter = document.querySelector('.cart_counter');
-let final_price = document.querySelector('.final_price');
+let product_list_div = document.querySelector(".product_list");
+let cart_counter = document.querySelector(".cart_counter");
+let final_price = document.querySelector(".final_price");
 
-let empty_cart_div = document.querySelector('.empty_cart');
-let cart_page_wrap_div = document.querySelector('.cart_page_wrap');
+let empty_cart_div = document.querySelector(".empty_cart");
+let cart_page_wrap_div = document.querySelector(".cart_page_wrap");
 
-let cart_object = JSON.parse(
-    localStorage.getItem('cart')
-);
+// some popup variables
+let pay_popup = document.querySelector(".popup");
+let html_body = document.getElementsByTagName("body")[0];
+let popup_close = document.querySelector(".popup_close");
+let cart_pay_btn = document.querySelector(".cart_pay_btn");
+
+let final_pay_btn = document.querySelector(".pay_btn");
+
+
+let cart_object = JSON.parse(localStorage.getItem("cart"));
 
 let item_template = `
 <div class="product_item">
@@ -21,62 +28,100 @@ let item_template = `
 </div>
 `;
 
-String.prototype.format = function() {
-    let formatted = this;
-    for (let i = 0; i < arguments.length; i++) {
-      let regexp = new RegExp('\\{'+i+'\\}', 'gi');
-      formatted = formatted.replace(regexp, arguments[i]);
-    }
-    return formatted;
+String.prototype.format = function () {
+  let formatted = this;
+  for (let i = 0; i < arguments.length; i++) {
+    let regexp = new RegExp("\\{" + i + "\\}", "gi");
+    formatted = formatted.replace(regexp, arguments[i]);
+  }
+  return formatted;
 };
 
 function evaluatePrice() {
-    let total_price = 0;
-    for (let product_id in cart_object) {
-        total_price += parseInt(cart_object[product_id].cost);
-    }
-    return total_price;
+  let total_price = 0;
+  for (let product_id in cart_object) {
+    total_price += parseInt(cart_object[product_id].cost);
+  }
+  return total_price;
+}
+
+function close_popup() {
+  pay_popup.classList.remove("active");
+  html_body.classList.remove("body_popup");
 }
 // -----------------
 
-
-
 if (cart_object != null && Object.keys(cart_object).length != 0) {
-    cart_page_wrap_div.classList.remove('deactive');
-    for (let product_id in cart_object) {
-        product_list_div.innerHTML += item_template.format(
-            product_id,
-            cart_object[product_id].title,
-            cart_object[product_id].cost
-        );
-    }
-    final_price.textContent = evaluatePrice();
+  cart_page_wrap_div.classList.remove("deactive");
+  for (let product_id in cart_object) {
+    product_list_div.innerHTML += item_template.format(
+      product_id,
+      cart_object[product_id].title,
+      cart_object[product_id].cost
+    );
+  }
+  final_price.textContent = evaluatePrice();
 
-    let remove_btns = document.querySelectorAll('.remove_div_a');
-    remove_btns.forEach((btn) => {
-        btn.addEventListener('click', (e) => {
-            e.preventDefault();
-            let product_div = btn.closest('.product_item');
-            let product_id = product_div.getElementsByTagName('span')[0].id;
-            delete cart_object[product_id];
-            localStorage.setItem(
-                'cart',
-                JSON.stringify(cart_object)
-            );
-            btn.closest('.product_item').remove();
+  let remove_btns = document.querySelectorAll(".remove_div_a");
+  remove_btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      let product_div = btn.closest(".product_item");
+      let product_id = product_div.getElementsByTagName("span")[0].id;
+      delete cart_object[product_id];
+      localStorage.setItem("cart", JSON.stringify(cart_object));
+      btn.closest(".product_item").remove();
 
-            cart_counter.innerText =
-                Object.keys(cart_object).length;
-            
-            final_price.textContent = evaluatePrice();
+      cart_counter.innerText = Object.keys(cart_object).length;
 
-            if (Object.keys(cart_object).length == 0) {
-                cart_page_wrap_div.classList.add('deactive');
-                empty_cart_div.classList.remove('deactive');
-            }
-        });
+      final_price.textContent = evaluatePrice();
+
+      if (Object.keys(cart_object).length == 0) {
+        cart_page_wrap_div.classList.add("deactive");
+        empty_cart_div.classList.remove("deactive");
+      }
     });
+  });
 } else {
-    empty_cart_div.classList.remove('deactive');
+  empty_cart_div.classList.remove("deactive");
 }
 
+cart_pay_btn.addEventListener("click", (e) => {
+  pay_popup.classList.add("active");
+  html_body.classList.add("body_popup");
+});
+
+popup_close.addEventListener("click", (e) => {
+  close_popup();
+});
+
+final_pay_btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let cart = JSON.parse(localStorage.getItem("cart"));
+  let pay_form = document.getElementById("pay_form");
+  let formData = new FormData(pay_form);
+
+  for (let product_id in cart) {
+    formData.append('products', product_id);
+  }
+
+  let resp = fetch('/cart/', {
+    body: formData,
+    method: 'POST'
+  });
+
+  console.log(resp);
+  // let list = document.querySelectorAll('#id_products > option');
+  
+  // for (let product_id in cart) {
+  //   for (let elem of list) {
+  //     if (elem.getAttribute('value') == product_id) {
+  //       elem.setAttribute('selected', '');
+  //       continue;
+  //     }
+  //   }
+  // }
+
+  // localStorage.removeItem('cart');
+  // pay_form.submit();
+});
