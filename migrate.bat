2cc8dae207d2330c01a8e43590f506946718d558
ruvl <ru.vlad.13@gmail.com>
2023-07-29 09:02:10 +0300

    fix migrate for rename venv

--- migrate.bat
+++ migrate.bat
@@ -1,9 +1,5 @@
-call .\venv\Scripts\activate.bat
-
 python manage.py makemigrations authapp && ^
 python manage.py makemigrations mainapp && ^
 python manage.py makemigrations paymentapp && ^
 python manage.py makemigrations && ^
 python manage.py migrate
-
-deactivate
