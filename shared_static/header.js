fa59d3837d2725b13a244b598e92c627387e39c0
ruvl <ru.vlad.13@gmail.com>
2023-08-09 17:58:14 +0300

    Fix DOMXSS problems in cart. Some adaptive fix.

--- shared_static/header.js
+++ shared_static/header.js
@@ -1,11 +1,7 @@
-window.onload = (e) => {
+// Cart counter
+window.addEventListener('DOMContentLoaded', () => {
     let cart_counter = document.querySelector('.cart_counter');
-
-    let cart_object = JSON.parse(
-        localStorage.getItem('cart')
-    );
-    if (cart_object != null) {
-        let i = Object.keys(cart_object).length;
-        cart_counter.innerText = i;
-    }
-};
+    let cart_object = JSON.parse(localStorage.getItem('cart'));
+    if (cart_object != null)
+        cart_counter.innerText = Object.keys(cart_object).length;
+})
