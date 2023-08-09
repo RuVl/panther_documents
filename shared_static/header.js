7c50ac9c346bef8d151a020ba715073a8d9157d4
Xamelllion <sid.vp04@gmail.com>
2023-08-10 00:23:46 +0300

    add header burger menu on mobile

--- shared_static/header.js
+++ shared_static/header.js
@@ -4,4 +4,11 @@ window.addEventListener('DOMContentLoaded', () => {
     let cart_object = JSON.parse(localStorage.getItem('cart'));
     if (cart_object != null)
         cart_counter.innerText = Object.keys(cart_object).length;
-})
+});
+
+let burger_menu = document.querySelector('.burger_menu');
+let vertical_menu = document.querySelector('.vertical_menu');
+
+burger_menu.onclick = () => {
+    vertical_menu.classList.toggle('hide_element_mobile');
+}
