6bf7f019aa8c5becdcaed547a68a3db0bf117651
ruvl <ru.vlad.13@gmail.com>
2023-08-21 21:55:39 +0300

    Fix main table hover. Move lang_switch to bottom of header

--- shared_static/components/lang_switch.js
+++ shared_static/components/lang_switch.js
@@ -1,17 +1,17 @@
 const form_input = document.getElementById('lang-picker');
 const selected = document.querySelector(".lang-selected");
 
-const optionsContainer = document.querySelector(".options-container");
+const lang_select = document.querySelector(".lang-select");
 const optionsList = document.querySelectorAll(".option");
 
 selected.addEventListener("click", () => {
-  optionsContainer.classList.toggle("active");
+  lang_select.classList.toggle("active");
 });
 
 optionsList.forEach(el => {
   el.addEventListener("click", () => {
-    selected.textContent = el.querySelector("label").textContent;
-    optionsContainer.classList.remove("active");
+    selected.querySelector('span').textContent = el.querySelector("label").textContent;
+    lang_select.classList.remove("active");
     form_input.value = el.querySelector('input').value;
     form_input.form.submit();
   });
